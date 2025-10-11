c7b920a288a57c63d183c4d7337e15e27bf30490
Ikwue Inalegwu Sipho <ikwueinalegwu@gmail.com>
2025-10-11 12:33:29 +0000

    [chore] responsiveness on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { Heading, Text } from "@radix-ui/themes";
+import { Flex, Heading, Text } from "@radix-ui/themes";
 import { allDocuments } from "contentlayer/generated";
+import { compareAsc } from "date-fns";
 import { Calendar } from "lucide-react";
 import { motion, useScroll, useTransform } from "motion/react";
 import { useEffect, useRef, useState } from "react";
 
 export default function Page() {
-  const timeline = allDocuments;
+  const timeline = allDocuments.sort((tA, tB) =>
+    compareAsc(new Date(tA.date), new Date(tB.date)),
+  );
 
   const ref = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
@@ -23,7 +26,7 @@ export default function Page() {
 
   const { scrollYProgress } = useScroll({
     target: containerRef,
-    // offset: ["start 10%", "end 50%"],
+    offset: ["start 10%", "end 50%"],
   });
 
   const heightTransform = useTransform(scrollYProgress, [0, 1], [0, height]);
@@ -32,54 +35,84 @@ export default function Page() {
   return (
     <div className="w-full" ref={containerRef}>
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <Heading size="8" className="mb-4 max-w-4xl">
-          Our Journey
-        </Heading>
-        <Text size="3" color="gray" className="max-w-sm">
-          We&apos;ve been working on the Ediga-Oya Rays of Hope Foundation for
-          the past 6 years. Here&apos;s a timeline of our journey.
-        </Text>
-      </div>
-      <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
-        {timeline.map((info) => (
-          <div
-            key={info._id}
-            className="flex justify-start pt-10 md:pt-40 md:gap-10"
-          >
-            <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-neutral-200 flex items-center justify-center">
-                <Calendar size={16} />
-              </div>
-              <Heading
-                as="h3"
-                className="md:block text-xl md:pl-20 md:text-5xl"
-              >
-                {info.item}
-              </Heading>
-            </div>
-            <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
-                {info.item}
-              </h3>
-              {info.description}{" "}
-            </div>
-          </div>
-        ))}
-        <div
-          style={{
-            height: `${height}px`,
+        <Flex
+          direction="column"
+          align={{
+            initial: "center",
+            md: "start",
+            lg: "start",
+            xl: "start",
           }}
-          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
         >
-          <motion.div
-            style={{
-              height: heightTransform,
-              opacity: opacityTransform,
+          <Heading size="8" className="mb-4 max-w-4xl">
+            Our Journey
+          </Heading>
+          <Text
+            size="3"
+            align={{
+              initial: "center",
+              md: "left",
+              lg: "left",
+              xl: "left",
             }}
-            className="absolute inset-x-0 top-0  w-[2px] bg-gradient-to-t from-purple-500 via-blue-500 to-transparent from-[0%] via-[10%] rounded-full"
-          />
-        </div>
+            color="gray"
+            className="max-w-sm"
+          >
+            We&apos;ve been working on the Ediga-Oya Rays of Hope Foundation for
+            the past 6 years. Here&apos;s a timeline of our journey.
+          </Text>
+        </Flex>
       </div>
+      <Flex
+        width="100%"
+        display={{
+          initial: "none",
+          md: "flex",
+          lg: "flex",
+          xl: "flex",
+        }}
+      >
+        <div ref={ref} className="relative max-w-7xl pb-20">
+          {timeline.map((info) => (
+            <div
+              key={info._id}
+              className="flex justify-start pt-10 md:pt-40 md:gap-10"
+            >
+              <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
+                <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-neutral-200 flex items-center justify-center">
+                  <Calendar size={16} />
+                </div>
+                <Heading
+                  as="h3"
+                  className="md:block text-xl md:pl-20 md:text-5xl"
+                >
+                  {info.item}
+                </Heading>
+              </div>
+              <div className="relative pl-20 pr-4 md:pl-4 w-full">
+                <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
+                  {info.item}
+                </h3>
+                {info.description}{" "}
+              </div>
+            </div>
+          ))}
+          <div
+            style={{
+              height: `${height}px`,
+            }}
+            className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+          >
+            <motion.div
+              style={{
+                height: heightTransform,
+                opacity: opacityTransform,
+              }}
+              className="absolute inset-x-0 top-0  w-[2px] bg-gradient-to-t from-teal-500 via-orange-200 to-transparent from-[0%] via-[10%] rounded-full"
+            />
+          </div>
+        </div>
+      </Flex>
     </div>
   );
 }
